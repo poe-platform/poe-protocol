7511d4e189872160f22e11e6c6f6f1bfb43ba56e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-06-05 13:16:22 -0700

    Improve battlebot

--- fastapi_poe/src/fastapi_poe/samples/concurrent_battle.py
+++ fastapi_poe/src/fastapi_poe/samples/concurrent_battle.py
@@ -6,6 +6,7 @@ Sample bot that returns results from both Sage and Claude-Instant.
 from __future__ import annotations
 
 import asyncio
+import re
 from collections import defaultdict
 from typing import AsyncIterable, AsyncIterator, Sequence
 
@@ -13,7 +14,9 @@ from sse_starlette.sse import ServerSentEvent
 
 from fastapi_poe import PoeBot, run
 from fastapi_poe.client import BotMessage, MetaMessage, stream_request
-from fastapi_poe.types import QueryRequest
+from fastapi_poe.types import ProtocolMessage, QueryRequest
+
+COMPARE_REGEX = r" ([A-Za-z_\-\d]+) vs\.? ([A-Za-z_\-\d]+)\s*$"
 
 
 async def advance_stream(
@@ -40,11 +43,51 @@ async def combine_streams(
                 yield label, msg
 
 
+def get_bots_to_compare(messages: Sequence[ProtocolMessage]) -> tuple[str, str]:
+    for message in reversed(messages):
+        if message.role != "user":
+            continue
+        match = re.search(COMPARE_REGEX, message.content)
+        if match is not None:
+            return match.groups()
+    return ("sage", "claude-instant")
+
+
+def preprocess_message(message: ProtocolMessage, bot: str) -> ProtocolMessage:
+    """Preprocess the conversation history.
+
+    For user messages, remove "x vs. y" from the end of the message.
+
+    For bot messages, try to keep only the parts of the message that come from
+    the bot we're querying.
+    """
+    if message.role == "user":
+        new_content = re.sub(COMPARE_REGEX, "", message.content)
+        return message.copy(update={"content": new_content})
+    elif message.role == "bot":
+        parts = re.split(r"\*\*([A-Za-z_\-\d]+)\*\* says:\n", message.content)
+        for message_bot, text in zip(parts[1::2], parts[2::2]):
+            if message_bot.casefold() == bot.casefold():
+                return message.copy(update={"content": text})
+        # If we can't find a message by this bot, just return the original message
+        return message
+    else:
+        return message
+
+
+def preprocess_query(query: QueryRequest, bot: str) -> QueryRequest:
+    new_query = query.copy(
+        update={"query": [preprocess_message(message, bot) for message in query.query]}
+    )
+    return new_query
+
+
 class ConcurrentBattleBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
+        bots = get_bots_to_compare(query.query)
         streams = [
-            (bot, stream_request(query, bot, "key"))
-            for bot in ("sage", "claude-instant")
+            (bot, stream_request(preprocess_query(query, bot), bot, "key"))
+            for bot in bots
         ]
         label_to_responses: dict[str, list[str]] = defaultdict(list)
         async for label, msg in combine_streams(streams):
